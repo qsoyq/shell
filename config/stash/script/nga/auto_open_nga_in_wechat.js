faefc5431588dbddf4c1aee7ff1ceff3318898d6
wangqs <wqs1639@gmail.com>
2024-03-26 20:52:56 +0800

    update

--- config/stash/script/nga/auto_open_nga_in_wechat.js
+++ config/stash/script/nga/auto_open_nga_in_wechat.js
@@ -5,20 +5,22 @@ const printObj = function(body){
 }
 
 function main(){
-    let url = $request.url
-    const match = url.match(/tid=(\d+)/);
-    const tid = match[1];
-    console.log(`url: ${$request.url}, tid: ${tid}`)
     // printObj($request.headers)
+    let url = $request.url
+    console.log(`url: ${$request.url}`)
     let ua = $request.headers["User-Agent"]
     const isFromWechat = /MicroMessenger/.test(ua);
     if(!isFromWechat){
         return
     }
-    let ngaUrl = encodeURIComponent(`nga://opentype=2?tid=${tid}&`)
-    let openUrl = `https://proxy-tool.19940731.xyz/api/network/url/redirect?url=${ngaUrl}`
-    
-    $notification.post("Nga", tid, openUrl, {url: openUrl})
+    const match = url.match(/tid=(\d+)/);
+    const tid = match ? match[1] : null    
+    if(!tid){
+        console.error(`获取 tid失败, 当前 url: ${url}`)
+    }
+    let ngaUrl = `nga://opentype=2?tid=${tid}&`
+    let openUrl = `https://proxy-tool.19940731.xyz/api/network/url/redirect?url=${encodeURIComponent(ngaUrl)}`
+    $notification.post("Nga", ngaUrl, ngaUrl, {url: openUrl})
 }
 
 main()
