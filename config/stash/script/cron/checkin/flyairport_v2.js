adfc08e6c7c1495db5f9915c50832649090d5816
wangqs <wqs1639@gmail.com>
2024-05-08 01:20:17 +0800

    update

--- config/stash/script/cron/checkin/flyairport_v2.js
+++ config/stash/script/cron/checkin/flyairport_v2.js
@@ -6,6 +6,21 @@ function getBodyArgument(key){
     return body[key]
 }
 
+function getLocalDateString(date){
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${year}-${month}-${day} ${hours}:${minutes}`
+}
+
+function ifRequest(){
+    let today = getLocalDateString(new Date())
+    let key = `${$script.name-today}`
+    return Boolean($persistentStore.read(key))
+}
+
 function main(){
     let email = getBodyArgument("email")
     if(!email){
@@ -24,17 +39,24 @@ function main(){
         email: email,
         passwd: passwd
     }
-    url = "https://proxy-tool.19940731.xyz/api/api/checkin/flyairport/v2"
-    $httpClient.post({url: url, headers: {"content-type":"application/json"}, body: JSON.stringify(payload)}, (error, _, data)=>{
-        if(error){
-            console.log(`请求失败 ${error}`)
-            $notification.post($script.name, "签到失败", error)
-        }else{
-            let body = JSON.parse(data)
-            console.log(`签到完成, ${body['msg']}`)
-        }
+    if (ifRequest()){
+        url = "https://proxy-tool.19940731.xyz/api/api/checkin/flyairport/v2"
+        $httpClient.post({url: url, headers: {"content-type":"application/json"}, body: JSON.stringify(payload)}, (error, _, data)=>{
+            if(error){
+                console.log(`请求失败 ${error}`)
+                $notification.post($script.name, "签到失败", error)
+            }else{
+                let body = JSON.parse(data)
+                console.log(`签到完成, ${body['msg']}`)
+                let key = `${$script.name-today}`
+                let today = getLocalDateString(new Date())
+                $persistentStore.write(today, key)
+            }
+            $done()
+        })
+    }else{
         $done()
-    })
+    }
 }
 
 main()
