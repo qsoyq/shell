11ca2e072db0ea0450ce8a2576d44258b09530c9
wangqs <wqs1639@gmail.com>
2024-05-13 02:37:34 +0800

    update

--- config/stash/script/cron/checkin/flyairport_v2.js
+++ config/stash/script/cron/checkin/flyairport_v2.js
@@ -10,9 +10,7 @@ function getLocalDateString(date){
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    return `${year}-${month}-${day} ${hours}:${minutes}`
+    return `${year}-${month}-${day}`
 }
 
 function ifRequest(){
