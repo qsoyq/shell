1e4e32c2bd002c10c15235f1afae3140b6a5d7bb
wangqs <wqs1639@gmail.com>
2024-08-18 06:45:01 +0800

    update

--- config/stash/script/weibo/goto_weibointernational_detail.js
+++ config/stash/script/weibo/goto_weibointernational_detail.js
@@ -1,64 +1,72 @@
 
-function getBodyArgument(key){
-    if (typeof $argument === "undefined"){
+function getBodyArgument(key) {
+    if (typeof $argument === "undefined") {
         return undefined
     }
     let body = JSON.parse($argument)
     return body[key]
 }
 
-
-const printObj = function(body){
-    for (const key in body) {
-        console.log(`    ==> Key: ${key}, Value: ${body[key]}`);
-    }    
-}
-
-function getUrlArgument(string, key){
+function getUrlArgument(string, key) {
     const url = new URL(string);
     const params = new URLSearchParams(url.searchParams);
     return params.get(key)
 }
 
-function getBlodid(url){
-    url = new URL(url)
-    let index = url.pathname.lastIndexOf('/')
-    if(index !== -1){
-        return url.pathname.substring(index + 1);
-    }
-    return null
-}
-
-function main(){
+async function main() {
     let url = $request.url
     console.log(`url: ${$request.url}`)
-    let blogid = getBlodid(url)
-    if(!blogid){
-        console.log(`获取 blogid 失败, 当前 url: ${url}`)
-    }
-    if(blogid === 'baiduad' || blogid === 'banner'){
-        console.log(`非预期 blogid, 跳过`)
-        $done({})
-        return 
-    }
-    let current = new Date().getTime()
-    let key = `${$script.name} - ${blogid}`
-    let lastSeen = $persistentStore.read(key)
-    console.log(`lastSeen ${blogid}: ${lastSeen}`)
-    if(lastSeen && (Number(current) - lastSeen) < (10*1000)){
-        console.log(`本次跳过`)
-    }else{
-        let urlscheme = `weibointernational://detail?mblogid=${blogid}`
-        // https://p.19940731.xyz/redoc#tag/network.url/operation/redirect_api_network_url_redirect_get
-        let openUrl = `https://p.19940731.xyz/api/network/url/redirect?url=${encodeURIComponent(urlscheme)}`
-        let disableOpenApp = getBodyArgument("disableOpenApp")
-        if(!disableOpenApp){
-            $notification.post("微博国际版", urlscheme, urlscheme, {url: openUrl})
+    // 根据 url 判断执行功能
+    let userMatch = url.match(/^https?:\/\/(m.weibo.cn|weibo.com)\/u\/(\d+)(.\w+=?\w*)?/)
+    if (userMatch) {
+        // 访问用户页面
+        let uid = userMatch[2]
+        let current = new Date().getTime()
+        let key = `${$script.name} - userMatch - ${uid}`
+        let lastSeen = $persistentStore.read(key)
+        console.log(`lastSeen ${uid}: ${lastSeen}`)
+        if (lastSeen && (Number(current) - lastSeen) < (10 * 1000)) {
+            console.log(`本次跳过`)
+        } else {
+            let urlscheme = `weibointernational://userinfo?uid=${uid}`
+            // https://p.19940731.xyz/redoc#tag/network.url/operation/redirect_api_network_url_redirect_get
+            let openUrl = `https://p.19940731.xyz/api/network/url/redirect?url=${encodeURIComponent(urlscheme)}`
+            let disableOpenApp = getBodyArgument("disableOpenApp")
+            if (!disableOpenApp) {
+                $notification.post("微博国际版", urlscheme, urlscheme, { url: openUrl })
+            }
+            $persistentStore.write(String(current), key)
         }
-        $persistentStore.write(String(current), key)
+        return
     }
 
-    $done({})
+    // 访问微博详情
+    let detailMatch = url.match(/^https:\/\/(weibo.com|m.weibo.cn)\/(status|\d+)\/(\w+|\d+)(.?.+)?$/)
+    if (detailMatch) {
+        let uid = detailMatch[3]
+        let current = new Date().getTime()
+        let key = `${$script.name} - blogMatch - ${uid}`
+        let lastSeen = $persistentStore.read(key)
+        console.log(`lastSeen ${uid}: ${lastSeen}`)
+        if (lastSeen && (Number(current) - lastSeen) < (10 * 1000)) {
+            console.log(`本次跳过`)
+        } else {
+            let urlscheme = `weibointernational://detail?mblogid=${uid}`
+            // https://p.19940731.xyz/redoc#tag/network.url/operation/redirect_api_network_url_redirect_get
+            let openUrl = `https://p.19940731.xyz/api/network/url/redirect?url=${encodeURIComponent(urlscheme)}`
+            let disableOpenApp = getBodyArgument("disableOpenApp")
+            if (!disableOpenApp) {
+                $notification.post("微博国际版", urlscheme, urlscheme, { url: openUrl })
+            }
+            $persistentStore.write(String(current), key)
+        }
+        return
+    }
+}
+try {
+    main()
+} catch (e) {
+    console.log(`error: ${e}`)
 }
 
-main()
+$done({})
