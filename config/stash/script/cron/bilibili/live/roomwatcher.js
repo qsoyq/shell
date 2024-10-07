d573c0dc250a7be53efd74a5a742585a4d4c78aa
wangqs <wqs1639@gmail.com>
2024-10-07 20:03:46 +0800

    update

--- config/stash/script/cron/bilibili/live/roomwatcher.js
+++ config/stash/script/cron/bilibili/live/roomwatcher.js
@@ -4,14 +4,14 @@ barkToken 为对应的推送令牌
 argument:
     {\"rooms\":\"1,2,3,4\",\"barkToken\":\"123456\"}
 */
-const printObj = function(body){
+const printObj = function (body) {
     for (const key in body) {
         console.log(`    ==> Key: ${key}, Value: ${body[key]}`);
-    }    
+    }
 }
 
-function getBodyArgument(key){
-    if (typeof $argument === "undefined"){
+function getBodyArgument(key) {
+    if (typeof $argument === "undefined") {
         return undefined
     }
     let body = JSON.parse($argument)
@@ -23,23 +23,23 @@ function removeHtmlTags(text) {
     const htmlTagsRe = /<[^>]+>/g;
     // 使用 replace 方法替换 HTML 标签，保留 title 里的字符串
     return text.replace(htmlTagsRe, (match) => {
-      const title = match.match(/title\s*=\s*"(.*?)"/);
-      return title ? title[1] : "";
+        const title = match.match(/title\s*=\s*"(.*?)"/);
+        return title ? title[1] : "";
     });
 }
-  
+
 function unescapeHtml(text) {
     const parser = new DOMParser();
     const doc = parser.parseFromString(text, "text/html");
     return doc.body.textContent;
 }
-    
-const getRoomList = function(){
+
+const getRoomList = function () {
     let rooms = getBodyArgument("rooms")
-    return rooms ? rooms.split(","): []
+    return rooms ? rooms.split(",") : []
 }
 
-const getBarkToken = function(){
+const getBarkToken = function () {
     return getBodyArgument("barkToken")
 }
 
@@ -48,7 +48,7 @@ const isDebug = Boolean(getBodyArgument("debug"))
 
 const isAlwaysPub = getBodyArgument("isAlwaysPub")
 
-function handler(roomId, ctx){
+function handler(roomId, ctx) {
     let lastPubKey = `${$script.name}lastPub${roomId}`
     let liveRoomLink = `https://live.bilibili.com/${roomId}`
     let url = `https://p.19940731.xyz/api/bilibili/live/room/${roomId}`
@@ -71,7 +71,7 @@ function handler(roomId, ctx){
         // 判断直播状态和上次推送时间
         let liveTime = new Date(roomInfo.live_time).getTime()
 
-        if (!isAlive){
+        if (!isAlive) {
             ctx.resolve(`${anchor.uname}的直播间${roomId}未开播`)
             return
         }
@@ -79,31 +79,31 @@ function handler(roomId, ctx){
         let current = new Date().getTime()
         let lastPub = $persistentStore.read(lastPubKey)
 
-        if (lastPub){
+        if (lastPub) {
             lastPub = Number(lastPub)
-            if(isDebug){
+            if (isDebug) {
                 console.log(`${anchor.uname} - lastPub: ${new Date(lastPub)}, liveTime: ${new Date(liveTime)}, current: ${new Date(current)}`)
-            }                
-            if(lastPub >= liveTime && !isAlwaysPub){
+            }
+            if (lastPub >= liveTime && !isAlwaysPub) {
                 ctx.resolve(`${anchor.uname}的直播间已在 ${new Date(lastPub)} 推送过`)
                 return
             }
         }
         // 推送   
-        const pushToStash = ()=>{
-            if(isDebug){
+        const pushToStash = () => {
+            if (isDebug) {
                 console.log(`${anchor.uname}\n${anchor.face}\n${roomInfo.title}\n\n${roomInfo.live_time}\n\n${liveRoomLink}`)
-            }                
-            $notification.post("bilibiliRoomLiveWatcher", `${anchor.uname}`, `${roomInfo.title}\n${roomInfo.live_time}\n${liveRoomLink}`,{url: liveRoomLink})
+            }
+            $notification.post("bilibiliRoomLiveWatcher", `${anchor.uname}`, `${roomInfo.title}\n${roomInfo.live_time}\n${liveRoomLink}`, { url: liveRoomLink })
             ctx.resolve(`${anchor.uname} ${roomId} 已开播`)
             $persistentStore.write(current.toString(), lastPubKey)
         }
 
-        const pushToBark = function(){
+        const pushToBark = function () {
             let barkToken = getBarkToken()
             let description = removeHtmlTags(unescapeHtml(roomInfo.description))
             let openLiveRoom = `bilibili://live/${roomId}`
-            let body =  `${roomInfo.title}\n${roomInfo.live_time}\n${description}`
+            let body = `${roomInfo.title}\n${roomInfo.live_time}\n${description}`
             body = body.substring(0, 1024)
             let payload = {
                 title: `${anchor.uname}`,
@@ -114,83 +114,84 @@ function handler(roomId, ctx){
                 url: openLiveRoom,
                 icon: anchor.face,
                 device_key: barkToken,
-                automaticallyCopy: "1"
-            }     
-            payload = JSON.stringify(payload)       
+                automaticallyCopy: "1",
+                level: "passive"
+            }
+            payload = JSON.stringify(payload)
             $httpClient.post({
                 url: "https://api.day.app/push",
-                headers: {"content-type": "application/json"},
+                headers: { "content-type": "application/json" },
                 body: payload
-            }, (error, response, data)=>{
-                if(isDebug){
+            }, (error, response, data) => {
+                if (isDebug) {
                     console.log(`为${anchor.uname}发送 bark 推送\nerror: ${error}\ndata:${data}`)
                 }
-                if(error || Number(response['status']) >= 300){
+                if (error || Number(response['status']) >= 300) {
                     console.log(`推送消息失败， ${anchor.uname} status: ${response['status']}, error: ${error}`)
                     ctx.reject(error)
-                }else{
+                } else {
                     ctx.resolve(`${anchor.uname} ${roomId} 已开播`)
                     $persistentStore.write(current.toString(), lastPubKey)
                 }
 
             })
-        }     
-        if(getBodyArgument("isPushToBark")){
-            pushToBark()    
         }
-        if(getBodyArgument("isPushToStash")){
+        if (getBodyArgument("isPushToBark")) {
+            pushToBark()
+        }
+        if (getBodyArgument("isPushToStash")) {
             pushToStash()
         }
     })
 }
 
-function main(){
-    if(isDebug){
+function main() {
+    if (isDebug) {
         printObj($environment)
-    
+
         let lastRunAtKey = `${$script.name}LastRunAt`
         let lastRunAt = $persistentStore.read(lastRunAtKey)
-        if (lastRunAt !== undefined){
+        if (lastRunAt !== undefined) {
             console.log(`lastRunAt: ${lastRunAt}`)
         }
         const now = new Date().toString()
         $persistentStore.write(now, lastRunAtKey)
     }
-    
+
 
     const roomList = getRoomList()
     console.log(`isAlwaysPub func: ${isAlwaysPub}`)
     let promiseList = []
-    if(isDebug){
+    if (isDebug) {
         let barkToken = getBarkToken()
         console.log(`roomList: ${roomList}`)
         console.log(`barkToken: ${barkToken}`)
     }
-    
-    roomList.forEach((roomId)=>{
+
+    roomList.forEach((roomId) => {
         task = new Promise((resolve, reject) => {
-            handler(roomId, {resolve: resolve, reject: reject})
+            handler(roomId, { resolve: resolve, reject: reject })
         });
-        promiseList.push(task)    
+        promiseList.push(task)
     })
-    
+
     const allPromise = Promise.all(promiseList);
-    
+
     allPromise
-    .then((results) => { 
-        console.log(`run successed`)
-        results.forEach((resolve)=>{
-            // console.log(resolve)
+        .then((results) => {
+            console.log(`run successed`)
+            results.forEach((resolve) => {
+                // console.log(resolve)
+            })
+            $done({})
         })
-        $done({})
-    })
-    .catch((error) => {
-        console.error(`run failed`); // 输出："失败！"
-        error.forEach((reject)=>{
-            // console.log(reject)
+        .catch((error) => {
+            console.error(`run failed`); // 输出："失败！"
+            error.forEach((reject) => {
+                // console.log(reject)
+            })
+            $done({})
         })
-        $done({})
-    })
 }
 
 main()
