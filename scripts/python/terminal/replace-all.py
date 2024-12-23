0e9b1cce0ee647ccd5c8ea24cf202457bcb93f57
wangqs <wqs1639@gmail.com>
2024-12-24 00:37:37 +0800

    update

--- scripts/python/terminal/replace-all.py
+++ scripts/python/terminal/replace-all.py
@@ -1,17 +1,19 @@
-import sys
 from rich import print
 import typer
+from pathlib import Path
 
 
 def main(
+    path: Path = typer.Argument(..., help="待处理的文件"),
     old: str = typer.Argument(..., help="替换前的字符串"),
     new: str = typer.Argument(..., help="替换后的字符串"),
 ):
     """
     从标准输入读取字符串替换后输出
     """
-    data = sys.stdin.read()
-    if data:
+    if path.exists() and path.is_file():
+        with path.open("r") as f:
+            data = f.read()
         data = data.replace(old, new)
         print(data)
 
