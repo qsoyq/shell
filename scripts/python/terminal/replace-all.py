1afb6cacfe97c8ac9701803f120d70142b3caf7a
wangqs <wqs1639@gmail.com>
2024-12-24 13:11:13 +0800

    update

--- scripts/python/terminal/replace-all.py
+++ scripts/python/terminal/replace-all.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 from rich import print
 import typer
-from pathlib import Path
 import sys
 
 
 def main(
-    # path: Path = typer.Argument(..., help="待处理的文件"),
     old: str = typer.Argument(..., help="替换前的字符串"),
     new: str = typer.Argument(..., help="替换后的字符串"),
 ):
@@ -17,11 +15,6 @@ def main(
     if data:
         data = data.replace(old, new)
         print(data)
-    # if path.exists() and path.is_file():
-    #     with path.open("r") as f:
-    #         data = f.read()
-    #     data = data.replace(old, new)
-    #     print(data)
 
 
 if __name__ == "__main__":
