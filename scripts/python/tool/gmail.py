e4ef87313309d4ff0024e897609bcce869f1ea56
wangqs <wqs1639@gmail.com>
2024-12-27 13:36:21 +0800

    update

--- scripts/python/tool/gmail.py
+++ scripts/python/tool/gmail.py
@@ -16,7 +16,7 @@ def gmail(
     gmail_password: str = typer.Option(..., "-p", help="应用专用密码"),
     recipient_email: str = typer.Option(..., "-r", help="收件人邮箱地址"),
     subject: str = typer.Option(..., "--subject", help="邮件主题"),
-    body: str = typer.Option(..., "--body", help="邮件正文"),
+    body: str = typer.Option(None, "--body", help="邮件正文"),
 ):
     """使用 Gmail 发送邮件
     应用专用密码设置: https://myaccount.google.com/apppasswords
@@ -30,13 +30,6 @@ def gmail(
     GMAIL_USER = gmail_user
     GMAIL_PASSWORD = gmail_password
 
-    # 收件人信息
-    recipient_email = recipient_email
-
-    # 邮件内容
-    subject = subject
-    body = body
-
     # 创建邮件对象
     message = MIMEMultipart()
     message["From"] = GMAIL_USER
@@ -44,7 +37,8 @@ def gmail(
     message["Subject"] = subject
 
     # 添加邮件正文
-    message.attach(MIMEText(body, "plain"))
+    if body is not None:
+        message.attach(MIMEText(body, "plain"))
 
     try:
         server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
@@ -54,11 +48,11 @@ def gmail(
         # 发送邮件
         server.sendmail(GMAIL_USER, recipient_email, message.as_string())
         print(f"[{now}] 邮件发送成功！")
-        typer.Exit(0)
+        raise typer.Exit(0)
 
     except Exception as e:
         print(f"[{now}] 邮件发送失败: {e}")
-        typer.Exit(-1)
+        raise typer.Exit(-1)
 
     finally:
         server.quit()
