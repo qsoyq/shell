703aa9cb03f093b7292cab515155c19890b0879a
wangqs <wqs1639@gmail.com>
2024-12-29 11:45:39 +0800

    update

--- scripts/bash/debug-tool.sh
+++ scripts/bash/debug-tool.sh
@@ -6,5 +6,5 @@ curl -s -o /usr/local/bin/replace-all "https://raw.githubusercontent.com/qsoyq/s
 curl -s -o /usr/local/bin/MarkdownInlineImageLinkMigrator "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/markdown/MarkdownInlineImageLinkMigrator.py?v=$ts"
 curl -s -o /usr/local/bin/gmail "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/tool/gmail.py?v=$ts"
 curl -s -o /usr/local/bin/tastien "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/crontab/tastien.py?v=$ts"
-curl -s -o /usr/local/bin/until "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/tool/until.py?v=$ts"
+curl -s -o /usr/local/bin/waitfor "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/tool/waitfor.py?v=$ts"
 chmod +x /usr/local/bin/*
