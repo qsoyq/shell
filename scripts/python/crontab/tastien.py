6f4d45cf4833ec005e3aa67c3b8bbadc08999dc3
wangqs <wqs1639@gmail.com>
2025-01-01 10:05:35 +0800

    update

--- scripts/python/crontab/tastien.py
+++ scripts/python/crontab/tastien.py
@@ -81,6 +81,7 @@ def point_create(
             "请勿重复提交",
             "当前时段可领取数量已经达到上限",
             "用户限领已达上限",
+            "该用户已经有锁定的记录",
         ]:
             if content in text:
                 echo(f"[Break] {content}")
