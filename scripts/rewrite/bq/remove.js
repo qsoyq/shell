f1db7a8d9f7c9e29fadee4457c6b056d869cdbb1
wangqs <wqs1639@gmail.com>
2025-01-01 18:01:00 +0800

    update

---
+++ scripts/rewrite/bq/remove.js
@@ -0,0 +1,13 @@
+(function () {
+    let link = document.getElementsByClassName("link")
+    if (link) {
+        link[0].remove()
+    }
+})();
+
+(function () {
+    let footer = document.getElementsByClassName("footer")
+    if (footer) {
+        footer[0].remove()
+    }
+})();
