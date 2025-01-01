e0a59c9f4cdbee6784c951c758e45a3e0ad465b6
wangqs <wqs1639@gmail.com>
2025-01-01 16:36:49 +0800

    update

---
+++ scripts/rewrite/bq/bg.js
@@ -0,0 +1,36 @@
+// 记忆网页护眼、关灯行为
+let lastBg = localStorage.getItem('.bq.lastBg')
+if (lastBg) {
+    // @ts-ignore
+    nr_setbg(lastBg)
+}
+
+(function () {
+    const element = document.getElementById('huyandiv');
+    if (element) {
+        element.addEventListener('click', function () {
+            let lastBg = localStorage.getItem('.bq.lastBg')
+            if (lastBg) {
+                localStorage.setItem('.bq.lastBg', "")
+            } else {
+                localStorage.setItem('.bq.lastBg', "huyan")
+            }
+            console.log('huyandiv was clicked!');
+        });
+    }
+})();
+
+(function () {
+    const element = document.getElementById('lightdiv');
+    if (element) {
+        element.addEventListener('click', function () {
+            let lastBg = localStorage.getItem('.bq.lastBg')
+            if (lastBg) {
+                localStorage.setItem('.bq.lastBg', "")
+            } else {
+                localStorage.setItem('.bq.lastBg', "light")
+            }
+            console.log('huyandiv was clicked!');
+        });
+    }
+})();
