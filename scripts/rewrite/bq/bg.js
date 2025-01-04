3b206504e68329a2fe12fc472b0acfc5fd907c0b
wangqs <wqs1639@gmail.com>
2025-01-05 00:08:34 +0800

    update

--- scripts/rewrite/bq/bg.js
+++ scripts/rewrite/bq/bg.js
@@ -1,9 +1,61 @@
+const default_style = `
+`;
+
+const huyan_style = `
+    body {
+        background-color: rgb(204, 232, 207) !important;
+    }
+    #chaptercontent {
+        color: #000000 !important;
+    }        
+
+    .Readpage .Readpage_up {
+        color: #000000 !important;
+    }
+    .Readpage .Readpage_down {
+        color: #000000 !important;
+    }
+
+    
+`;
+
+const lighton_style = `
+    body {
+        background: rgb(3, 3, 3) !important;
+    }
+
+    .Readpage {
+        background: rgb(3, 3, 3) !important;
+        color: rgb(3, 3, 3) !important;
+    }
+
+    .header {
+        background: rgb(3, 3, 3) !important;
+    }                            
+
+    #chaptercontent {
+        color: rgb(153, 153, 153);
+    }
+`;
+
 // 记忆网页护眼、关灯行为
 (function () {
     let lastBg = localStorage.getItem('.bq.lastBg')
     if (lastBg) {
         // @ts-ignore
-        nr_setbg(lastBg)
+        // nr_setbg(lastBg)
+        // 创建样式表
+        const style = document.createElement('style');
+        style.id = "myStyle"
+        if (lastBg === "huyan") {
+            style.innerHTML = huyan_style
+        } else if (lastBg === 'light') {
+            style.innerHTML = lighton_style
+        } else {
+            style.innerHTML = default_style
+        }
+        // 将样式表添加到文档中
+        document.head.appendChild(style);
     }
 })();
 
@@ -13,10 +65,19 @@
     if (element) {
         element.addEventListener('click', function () {
             let lastBg = localStorage.getItem('.bq.lastBg')
+            let style = document.getElementById("myStyle")
             if (lastBg !== 'huyan') {
                 localStorage.setItem('.bq.lastBg', "huyan")
+                if (style) {
+                    style.innerHTML = huyan_style
+                    console.log(`change sytle to huyan_style`)
+                }
             } else {
                 localStorage.setItem('.bq.lastBg', "")
+                if (style) {
+                    style.innerHTML = default_style
+                    console.log(`change sytle to default_style`)
+                }
             }
             console.log('huyandiv was clicked!');
         });
@@ -28,10 +89,19 @@
     if (element) {
         element.addEventListener('click', function () {
             let lastBg = localStorage.getItem('.bq.lastBg')
+            let style = document.getElementById("myStyle")
             if (lastBg !== 'light') {
                 localStorage.setItem('.bq.lastBg', "light")
+                if (style) {
+                    style.innerHTML = lighton_style
+                    console.log(`change sytle to lighton_style`)
+                }
             } else {
                 localStorage.setItem('.bq.lastBg', "")
+                if (style) {
+                    style.innerHTML = default_style
+                    console.log(`change sytle to default_style`)
+                }
             }
             console.log('lightdiv was clicked!');
         });
--- scripts/rewrite/bq/css.js
+++ scripts/rewrite/bq/css.js
@@ -1,16 +1,23 @@
+
+
+
 (function () {
     let lastBg = localStorage.getItem('.bq.lastBg')
     if (lastBg) {
         // 创建样式表
         const style = document.createElement('style');
         style.type = 'text/css';
-        style.innerHTML = `
-            #read {
-                background-color: rgb(17, 17, 17);
-            }
-        `;
-
+        style.id = "myStyle"
+        if (lastBg === "huyan") {
+            style.innerHTML = huyan_style
+        } else if (lastBg === 'light') {
+            style.innerHTML = lighton_style
+        } else {
+            style.innerHTML = default_style
+        }
         // 将样式表添加到文档中
         document.head.appendChild(style);
     }
 })();
+
+console.log('deprecated')
