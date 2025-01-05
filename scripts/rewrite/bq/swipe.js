e391196b4f0574340a066b6c753fc4a0e3d43968
wangqs <wqs1639@gmail.com>
2025-01-05 17:01:58 +0800

    update

--- scripts/rewrite/bq/swipe.js
+++ scripts/rewrite/bq/swipe.js
@@ -17,33 +17,32 @@ function gotoNext() {
     }
 })();
 
-(function () {
-    console.log('swipe')
-    let chaptercontent = document.getElementById('chaptercontent')
-    if (!chaptercontent) {
-        return
-    }
-    let startX
-    chaptercontent.addEventListener('touchstart', (event) => {
-        startX = event.touches[0].clientX; // 记录起始触摸点的 X 坐标
-    });
+// (function () {
+//     console.log('swipe')
+//     let chaptercontent = document.getElementById('chaptercontent')
+//     if (!chaptercontent) {
+//         return
+//     }
+//     let startX
+//     chaptercontent.addEventListener('touchstart', (event) => {
+//         startX = event.touches[0].clientX; // 记录起始触摸点的 X 坐标
+//     });
 
-    chaptercontent.addEventListener('touchmove', (event) => {
-        // 这里可以处理滑动过程中的逻辑
-    });
+//     chaptercontent.addEventListener('touchmove', (event) => {
+//         // 这里可以处理滑动过程中的逻辑
+//     });
 
-    chaptercontent.addEventListener('touchend', (event) => {
-        const endX = event.changedTouches[0].clientX; // 记录结束触摸点的 X 坐标
-        const distance = endX - startX; // 计算滑动距离
+//     chaptercontent.addEventListener('touchend', (event) => {
+//         const endX = event.changedTouches[0].clientX; // 记录结束触摸点的 X 坐标
+//         const distance = endX - startX; // 计算滑动距离
 
-        if (distance > 200) {
-            // 向右滑动
-            console.log('Swiped Right!');
-        } else if (distance < -200) {
-            // 向左滑动
-            gotoNext()
-            console.log('Swiped Left!');
-        }
-    });
-})();
+//         if (distance > 200) {
+//             // 向右滑动
+//             console.log('Swiped Right!');
+//         } else if (distance < -200) {
+//             // 向左滑动
+//             console.log('Swiped Left!');
+//         }
+//     });
+// })();
 
