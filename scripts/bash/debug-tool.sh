b417461e347040f7e0a7a3ea396936ee27f864f9
wangqs <wqs1639@gmail.com>
2025-01-06 15:56:55 +0800

    update

--- scripts/bash/debug-tool.sh
+++ scripts/bash/debug-tool.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-pip --quiet install httpx bs4 typer rich tabulate socksio
-curl -s -o /usr/local/bin/mikanani-magnet "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/mikanani/filter.py?v=$(date +%s)"
-curl -s -o /usr/local/bin/replace-all "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/terminal/replace-all.py?v=$(date +%s)"
-curl -s -o /usr/local/bin/MarkdownInlineImageLinkMigrator "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/markdown/MarkdownInlineImageLinkMigrator.py?v=$(date +%s)"
-curl -s -o /usr/local/bin/gmail "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/tool/gmail.py?v=$(date +%s)"
-curl -s -o /usr/local/bin/tastien "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/crontab/tastien.py?v=$(date +%s)"
-curl -s -o /usr/local/bin/waitfor "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/tool/waitfor.py?v=$(date +%s)"
-curl -s -o /usr/local/bin/pytar "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/tool/ptar.py?v=$(date +%s)"
-curl -s -o /usr/local/bin/duplicate "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/tool/duplicate.py?v=$(date +%s)"
-curl -s -o /usr/local/bin/markdown-table "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/markdown/table.py?v=$(date +%s)"
+pip --quiet install httpx bs4 typer rich tabulate socksio ipython
+curl -s -o /usr/local/bin/mikanani-magnet "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/mikanani/filter.py?v=$(date +%s)"
+curl -s -o /usr/local/bin/replace-all "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/terminal/replace-all.py?v=$(date +%s)"
+curl -s -o /usr/local/bin/MarkdownInlineImageLinkMigrator "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/markdown/MarkdownInlineImageLinkMigrator.py?v=$(date +%s)"
+curl -s -o /usr/local/bin/gmail "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/tool/gmail.py?v=$(date +%s)"
+curl -s -o /usr/local/bin/tastien "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/crontab/tastien.py?v=$(date +%s)"
+curl -s -o /usr/local/bin/waitfor "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/tool/waitfor.py?v=$(date +%s)"
+curl -s -o /usr/local/bin/pytar "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/tool/ptar.py?v=$(date +%s)"
+curl -s -o /usr/local/bin/duplicate "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/tool/duplicate.py?v=$(date +%s)"
+curl -s -o /usr/local/bin/markdown-table "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/markdown/table.py?v=$(date +%s)"
+curl -s -o /usr/local/bin/clear-docker-log "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/tool/clear-docker-log.py?v=$(date +%s)"
 
 chmod +x /usr/local/bin/*
---
+++ scripts/python/tool/clear-docker-log.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import os
+import rich
+from datetime import datetime
+import typer
+from pathlib import Path
+
+
+app = typer.Typer()
+
+
+def echo(*args, **kwargs):
+    if args:
+        arg = f"[{get_current_datetime_str()}] {args[0]}"
+        args = [arg, *args[1:]]
+    rich.print(*args, **kwargs)
+
+
+def get_current_datetime_str() -> str:
+    return datetime.now().strftime(r"%Y-%m-%d %H:%M:%S")
+
+
+@app.command()
+def main():
+    p = Path("/var/lib/docker/containers")
+    for d in p.iterdir():
+        if d.is_dir():
+            for f in d.rglob("*.log"):
+                echo(f)
+                cmd = f"truncate -s 0 {f.absolute()}"
+                os.system(cmd)
+
+
+if __name__ == "__main__":
+    app()
