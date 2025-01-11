86a40d44e152025fc192b5f05efd26157826482e
wangqs <wqs1639@gmail.com>
2025-01-11 22:41:54 +0800

    update

--- scripts/python/clash/dump_connections.py
+++ scripts/python/clash/dump_connections.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python3
+
 import time
 import httpx
 import json
 from pathlib import Path
-import os
 
-url = os.getenv("url", "http://127.0.0.1:9090/connections")
-output = "./connections.json"
+import rich
+from datetime import datetime
+import typer
 
-if __name__ == "__main__":
+
+app = typer.Typer()
+
+
+def echo(*args, **kwargs):
+    if args:
+        arg = f"[{get_current_datetime_str()}] {args[0]}"
+        args = [arg, *args[1:]]
+    rich.print(*args, **kwargs)
+
+
+def get_current_datetime_str() -> str:
+    return datetime.now().strftime(r"%Y-%m-%d %H:%M:%S")
+
+
+@app.command()
+def main(
+    url: str = typer.Option(
+        "http://127.0.0.1:9090/connections",
+        "-u",
+        "--url",
+        help="clash connections api endpoint",
+    ),
+    output: Path = typer.Option(
+        "./connections.json", "-o", "--output", help="输出路径"
+    ),
+    interval: float = typer.Option(1, "--interval", help="连接数据写入文件的间隔时间"),
+):
     result = set()
-    path = Path(output)
     while True:
         res = httpx.get(url)
         data = res.json()
@@ -36,8 +63,11 @@ if __name__ == "__main__":
                     chain,
                 )
             )
+        out = [list(x) for x in result]
+        data = json.dumps({"connections": out}, ensure_ascii=False, indent=4)
+        output.write_text(data)
+        time.sleep(interval)
 
-        with path.open("w") as f:
-            out = [list(x) for x in result]
-            f.write(json.dumps({"connections": out}, ensure_ascii=False, indent=4))
-        time.sleep(1)
+
+if __name__ == "__main__":
+    app()
