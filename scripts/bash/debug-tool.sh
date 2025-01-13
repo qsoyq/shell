b826a50b628c61a7c2b9d41891e98f601822edd4
wangqs <wqs1639@gmail.com>
2025-01-14 05:11:03 +0800

    update

--- scripts/bash/debug-tool.sh
+++ scripts/bash/debug-tool.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip install --upgrade certifi   --trusted-host pypi.org --trusted-host files.pythonhosted.org
-pip install httpx bs4 typer rich tabulate socksio ipython pysocks ipython pyvim ruff you-get vcron --trusted-host pypi.org --trusted-host files.pythonhosted.org
+pip install PyYAML httpx bs4 typer rich tabulate socksio ipython pysocks ipython pyvim ruff you-get vcron --trusted-host pypi.org --trusted-host files.pythonhosted.org
 
 curl -sL -o /usr/local/bin/mikanani-magnet "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/mikanani/filter.py?v=$(date +%s)"
 curl -sL -o /usr/local/bin/replace-all "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/python/terminal/replace-all.py?v=$(date +%s)"
@@ -18,11 +18,4 @@ curl -sL -o /usr/local/bin/pudp "https://raw.githubusercontent.com/qsoyq/shell/m
 
 chmod +x /usr/local/bin/*
 
-# alias
-alias unicode-escape='python3 -c "import sys;data=sys.stdin.read();print(data.encode(\"unicode-escape\").decode(),end=\"\")"'
-alias unicode-deescape='python3 -c "import sys;data=sys.stdin.read();print(data.encode(\"utf-8\").decode(\"unicode_escape\"),end=\"\")"'
-alias dump_clash_connections="curl -s https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/clash/dump_connections.py | python3"
-alias dump-http-capture="curl -s https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/stash/dump-http-capture.py | python3" 
-alias markdown-image-move="markdown_inline_image_link_oss_export && MarkdownInlineImageLinkMigrator -d ~/Documents/obsidian/Obsidian"
-alias json-to-yaml="python3 -c \"import sys,json,yaml;data=sys.stdin.read();print(yaml.safe_dump(json.loads(data)))\""
-alias yaml-to-json="python3 -c \"import sys,json,yaml;data=sys.stdin.read();print(json.dumps(yaml.safe_load(data), ensure_ascii=False, indent=4))\""
+
--- zshrc/debug.zshrc
+++ zshrc/debug.zshrc
@@ -1,1 +1,10 @@
 alias uc='curl -sL "https://raw.githubusercontent.com/qsoyq/shell/refs/heads/main/scripts/bash/debug-tool.sh?v=$(date +%s)" | bash'
+
+# alias
+alias unicode-escape='python3 -c "import sys;data=sys.stdin.read();print(data.encode(\"unicode-escape\").decode(),end=\"\")"'
+alias unicode-deescape='python3 -c "import sys;data=sys.stdin.read();print(data.encode(\"utf-8\").decode(\"unicode_escape\"),end=\"\")"'
+alias dump_clash_connections="curl -s https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/clash/dump_connections.py | python3"
+alias dump-http-capture="curl -s https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/stash/dump-http-capture.py | python3" 
+alias markdown-image-move="markdown_inline_image_link_oss_export && MarkdownInlineImageLinkMigrator -d ~/Documents/obsidian/Obsidian"
+alias json-to-yaml="python3 -c \"import sys,json,yaml;data=sys.stdin.read();print(yaml.safe_dump(json.loads(data)))\""
+alias yaml-to-json="python3 -c \"import sys,json,yaml;data=sys.stdin.read();print(json.dumps(yaml.safe_load(data), ensure_ascii=False, indent=4))\""
