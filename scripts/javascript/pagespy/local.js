6e56d7c0c4b8ec1fe0142ee640f08b434f869c2b
qsoyq <github@19940731.xyz>
2025-01-20 04:22:18 +0800

    update

--- scripts/javascript/pagespy/local.js
+++ scripts/javascript/pagespy/local.js
@@ -1,7 +1,7 @@
 var script1 = document.createElement('script');
 script1.setAttribute('crossorigin', 'anonymous');
 // 实际项目中请替换 SDK 的地址连接
-script1.src = 'http://pagespy.docker.localhost/page-spy/index.min.js';
+script1.src = 'https://pagespy.19940731.xyz/page-spy/index.min.js';
 
 var script2 = document.createElement('script');
 script2.textContent = 'window.$pageSpy = new PageSpy();';
