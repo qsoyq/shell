c16528861f8f813dafae65543f820c3f41d6825a
qsoyq <github@19940731.xyz>
2025-03-13 18:13:18 +0800

    update

--- config/stash/script/ad/x.home.js
+++ config/stash/script/ad/x.home.js
@@ -1,112 +1,14 @@
-/**
- * 在返回的 html 文本中添加 script 标签执行对应代码
- * 支持传入 url 通过 createElement 生成对象
- * 支持传入代码通过 createElement 生成对象
- * 支持指定在head、body、
- */
-
-// https://github.com/EtherDream/str2gbk
-let table
-
-function initGbkTable() {
-    // https://en.wikipedia.org/wiki/GBK_(character_encoding)#Encoding
-    const ranges = [
-        [0xA1, 0xA9, 0xA1, 0xFE],
-        [0xB0, 0xF7, 0xA1, 0xFE],
-        [0x81, 0xA0, 0x40, 0xFE],
-        [0xAA, 0xFE, 0x40, 0xA0],
-        [0xA8, 0xA9, 0x40, 0xA0],
-        [0xAA, 0xAF, 0xA1, 0xFE],
-        [0xF8, 0xFE, 0xA1, 0xFE],
-        [0xA1, 0xA7, 0x40, 0xA0],
-    ]
-    const codes = new Uint16Array(23940)
-    let i = 0
-
-    for (const [b1Begin, b1End, b2Begin, b2End] of ranges) {
-        for (let b2 = b2Begin; b2 <= b2End; b2++) {
-            if (b2 !== 0x7F) {
-                for (let b1 = b1Begin; b1 <= b1End; b1++) {
-                    codes[i++] = b2 << 8 | b1
-                }
-            }
-        }
-    }
-    table = new Uint16Array(65536)
-    table.fill(0xFFFF)
-
-    const str = new TextDecoder('gbk').decode(codes)
-    for (let i = 0; i < str.length; i++) {
-        table[str.charCodeAt(i)] = codes[i]
-    }
-}
-
-
-// @ts-ignore
-const NodeJsBufAlloc = typeof Buffer === 'function' && Buffer.allocUnsafe
-
-const defaultOnAlloc = NodeJsBufAlloc
-    ? (/** @type {any} */ len) => NodeJsBufAlloc(len)
-    : (/** @type {Iterable<number>} */ len) => new Uint8Array(len)
-
-const defaultOnError = () => 63   // '?'
-
+/** @namespace x.home */
 
 /**
- * @param {string} str
- */
-function str2gbk(str, opt = {}) {
-    if (!table) {
-        initGbkTable()
-    }
-    const onAlloc = opt.onAlloc || defaultOnAlloc
-    const onError = opt.onError || defaultOnError
-
-    const buf = onAlloc(str.length * 2)
-    let n = 0
-
-    for (let i = 0; i < str.length; i++) {
-        const code = str.charCodeAt(i)
-        if (code < 0x80) {
-            buf[n++] = code
-            continue
-        }
-        const gbk = table[code]
-
-        if (gbk !== 0xFFFF) {
-            buf[n++] = gbk
-            buf[n++] = gbk >> 8
-        } else if (code === 8364) {
-            // 8364 == '€'.charCodeAt(0)
-            // Code Page 936 has a single-byte euro sign at 0x80
-            buf[n++] = 0x80
-        } else {
-            const ret = onError(i, str)
-            if (ret === -1) {
-                break
-            }
-            if (ret > 0xFF) {
-                buf[n++] = ret
-                buf[n++] = ret >> 8
-            } else {
-                buf[n++] = ret
-            }
-        }
-    }
-    return buf.subarray(0, n)
-}
-
-/** @namespace html.add.script */
-
-/**
- * @typedef {Object} html.add.script.HTTPResponse
+ * @typedef {Object} x.home.HTTPResponse
  * @property {string|null} error - 错误信息，如果没有错误则为 null
  * @property {object} response - HTTP 响应对象
  * @property {string|null} data - 返回的数据，如果没有数据则为 null
  */
 
 /**
- * @typedef {function(Error|string|null, Object, string|null): void} html.add.script.HTTPCallback
+ * @typedef {function(Error|string|null, Object, string|null): void} x.home.HTTPCallback
  * 回调函数类型，接受错误、响应和数据作为参数。
  * @param {Error|string|null} error - 错误信息，可以是 Error 对象、字符串或者 null
  * @param {Object} response - HTTP 响应对象
@@ -114,18 +16,18 @@ function str2gbk(str, opt = {}) {
  */
 
 /**
- * @typedef {function(Object, html.add.script.HTTPCallback): html.add.script.HTTPResponse} html.add.script.HTTPMethod
+ * @typedef {function(Object, x.home.HTTPCallback): x.home.HTTPResponse} x.home.HTTPMethod
  */
 
 /**
- * @typedef {Object} html.add.script.HttpClient
- * @property {html.add.script.HTTPMethod} get - 发送 GET 请求
- * @property {html.add.script.HTTPMethod} post - 发送 POST 请求
- * @property {html.add.script.HTTPMethod} put - 发送 PUT 请求
- * @property {html.add.script.HTTPMethod} delete - 发送 DELETE 请求
+ * @typedef {Object} x.home.HttpClient
+ * @property {x.home.HTTPMethod} get - 发送 GET 请求
+ * @property {x.home.HTTPMethod} post - 发送 POST 请求
+ * @property {x.home.HTTPMethod} put - 发送 PUT 请求
+ * @property {x.home.HTTPMethod} delete - 发送 DELETE 请求
  */
 
-/** @type {html.add.script.HttpClient} */
+/** @type {x.home.HttpClient} */
 var $httpClient;
 
 var $request, $response, $notification, $argument, $persistentStore, $script
@@ -137,12 +39,12 @@ var $done
  * 对异步回调的 HTTP 调用包装成 async 函数
  * @param {'GET'|'POST'|'PUT'|'DELETE'} method - HTTP 方法类型，支持 GET、POST、PUT 和 DELETE
  * @param {Object} params - 请求参数对象，包含请求所需的各类信息
- * @returns {Promise<html.add.script.HTTPResponse>} 返回一个 Promise，解析为包含 error、response 和 data 的对象
+ * @returns {Promise<x.home.HTTPResponse>} 返回一个 Promise，解析为包含 error、response 和 data 的对象
  * @throws {Error} 如果请求失败，Promise 会被拒绝并返回错误信息
  */
 async function request(method, params) {
     return new Promise((resolve, reject) => {
-        /** @type {html.add.script.HTTPMethod} */
+        /** @type {x.home.HTTPMethod} */
         const httpMethod = $httpClient[method.toLowerCase()]; // 通过 HTTP 方法选择对应的请求函数
         httpMethod(params, (error, response, data) => {
             if (error) {
@@ -158,7 +60,7 @@ async function request(method, params) {
 /**
  * 请求封装
  * @param {object} params
- * @returns {Promise<html.add.script.HTTPResponse>}
+ * @returns {Promise<x.home.HTTPResponse>}
  */
 async function get(params) {
     return request('GET', params);
@@ -167,7 +69,7 @@ async function get(params) {
 /**
  * 请求封装
  * @param {object} params
- * @returns {Promise<html.add.script.HTTPResponse>}
+ * @returns {Promise<x.home.HTTPResponse>}
  */
 async function post(params) {
     return request('POST', params);
@@ -176,7 +78,7 @@ async function post(params) {
 /**
  * 请求封装
  * @param {object} params
- * @returns {Promise<html.add.script.HTTPResponse>}
+ * @returns {Promise<x.home.HTTPResponse>}
  */
 async function put(params) {
     return request('PUT', params);
@@ -185,7 +87,7 @@ async function put(params) {
 /**
  * 请求封装
  * @param {object} params
- * @returns {Promise<html.add.script.HTTPResponse>}
+ * @returns {Promise<x.home.HTTPResponse>}
  */
 async function delete_(params) {
     return request('DELETE', params);
@@ -259,7 +161,7 @@ function setCookie(key, val) {
  * @param {string} title 
  * @param {string} subtitle 
  * @param {string} content 
- * @param {string|undefined} [url] 
+ * @param {string|undefined} url 
  */
 function notificationPost(title, subtitle, content, url) {
     const params = url ? { url } : {};
@@ -294,11 +196,11 @@ function randomChar(num) {
 
 /**
  * 将指定日期对象转为相应的日期时间字符串
- * @param {Date} date 
+ * @param {Date|null} [date=null] 
  * @returns {string} 表示当前时间的字符串
  */
-function getLocalDateString(date) {
-    if (typeof date === 'undefined') {
+function getLocalDateString(date = null) {
+    if (!date) {
         date = new Date()
     }
     const year = date.getFullYear();
@@ -309,6 +211,8 @@ function getLocalDateString(date) {
     const seconds = date.getSeconds()
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
 }
+
+
 /**
  * 遍历并输出对象字面值
  * @param {object} body 
@@ -345,7 +249,7 @@ function parseJsonBody(string) {
     try {
         return JSON.parse(string)
     } catch (e) {
-        console.log(`invalid json: ${e}, json: ${string}`)
+        console.log(`invalid json: ${e}\nraw: ${string}`)
         return null
     }
 }
@@ -360,14 +264,11 @@ function getScriptArgument(key) {
         return;
     }
 
-    let body;
-    try {
-        body = JSON.parse($argument);
-    } catch (error) {
-        console.log(`Invalid Argument JSON: ${error}, argument: ${$argument}`);
-        return null; // JSON 解析失败返回 null
+    let body = parseJsonBody($argument)
+    if (body) {
+        return body[key]
     }
-    return body[key]
+    return null
 }
 
 /**
@@ -432,119 +333,42 @@ function countryCodeToEmoji(countryCode) {
     // 将Unicode字符转换为emoji
     return String.fromCodePoint(...codePoints);
 }
-/**
- * 返回从 from 到 to 递增或递减的数组，步长为 1
- * @param {number} from 
- * @param {number} to 
- * @returns 
- */
-function generateArray(from, to) {
-    const start = Math.min(from, to);
-    const end = Math.max(from, to);
-
-    // 如果 from 大于 to，生成逆序数组
-    if (from > to) {
-        return Array.from({ length: end - start + 1 }, (_, i) => end - i);
-    } else {
-        // 否则生成顺序数组
-        return Array.from({ length: end - start + 1 }, (_, i) => start + i);
-    }
-}
 
 /**
- * 解析响应脚本参数
+ * 
  * @returns {string | undefined}
  */
-function getScriptResponseBody(encoding = "utf-8") {
-    let body = (typeof $response.body === 'object') ? (new TextDecoder(encoding)).decode(new Uint8Array($response.body)) : $response.body;
+function getScriptResponseBody() {
+    let body = (typeof $response.body === 'object') ? (new TextDecoder('utf-8')).decode(new Uint8Array($response.body)) : $response.body;
     return body
 }
 
 /**
- *  处理 telegram.sendMessage MarkdownV2 格式消息体转义
- * @param {string} text 
+ * 在指定作用域中执行代码
+ * @param {*} code 执行代码
+ * @param {*} context 上下文作用域
  * @returns 
  */
-function telegramEscapeMarkdownV2(text) {
-    const escapeChars = [
-        { char: '_', replacement: '\\_' },
-        { char: '*', replacement: '\\*' },
-        { char: '[', replacement: '\\[' },
-        { char: ']', replacement: '\\]' },
-        { char: '(', replacement: '\\(' },
-        { char: ')', replacement: '\\)' },
-        { char: '~', replacement: '\\~' },
-        { char: '>', replacement: '\\>' },
-        { char: '#', replacement: '\\#' },
-        { char: '+', replacement: '\\+' },
-        { char: '-', replacement: '\\-' },
-        { char: '=', replacement: '\\=' },
-        { char: '|', replacement: '\\|' },
-        { char: '{', replacement: '\\{' },
-        { char: '}', replacement: '\\}' },
-        { char: '.', replacement: '\\.' },
-        { char: '!', replacement: '\\!' },
-        { char: '`', replacement: '\\`' }
-    ];
-
-    let escapedText = text;
-
-    escapeChars.forEach(({ char, replacement }) => {
-        const regex = new RegExp(`\\${char}`, 'g');
-        escapedText = escapedText.replace(regex, replacement);
-    });
-
-    return escapedText;
+function safeEval(code, context) {
+    const func = new Function(...Object.keys(context), code);
+    return func(...Object.values(context));
 }
 
 async function main() {
-    let type = getScriptType()
-    let encoding = getScriptArgument('encoding') || 'utf-8'
-    switch (type) {
-        case "response":
-            let contentType = $response.headers["Content-Type"]
-            let isGbk = (contentType || "").toLowerCase().includes("gbk") ? true : false
-            let body = isGbk ? getScriptResponseBody('gbk') : getScriptResponseBody()
-            if (body && typeof contentType === 'string' && contentType.includes("text/html")) {
-                let domParser = new DOMParser();
-                let document = domParser.parseFromString(body, 'text/html');
-                let scripts = getScriptArgument("scripts") || []
-                for (const script of scripts) {
-                    let scriptTag = document.createElement('script');
-                    let parentTag = script?.parent || "body"
-                    if (script.async) {
-                        scriptTag.async = script.async
-                    }
-                    if (script.src) {
-                        scriptTag.src = script.src
-                    }
-                    if (script.textContent) {
-                        scriptTag.textContent = script.textContent
-                    }
-                    scriptTag.type = 'text/javascript'
-                    document[parentTag].appendChild(scriptTag);
-                }
-                let outerHTML = encoding.toLowerCase() == 'gbk' ? str2gbk(document.documentElement.outerHTML) : document.documentElement.outerHTML
-                $done({ body: outerHTML })
-            } else {
-                console.log(`[Response] [Headers]: ${JSON.stringify($response.headers)}`)
-                visitAll($response.headers)
-                console.log(`[Warn] invalid response body, content-type: ${$response.headers["Content-Type"]}`)
-                $done({})
-            }
-            break
-        default:
-            $done({})
-            break;
-    }
-
-
+    let body = getScriptResponseBody()
+    if (!body) { return }
+    let domParser = new DOMParser();
+    document = domParser.parseFromString(body, 'text/html');
+    let node = document.createElement("script")
+    node.textContent = "console.log(x.home.v3)"
+    document.head.appendChild(node)
+    let outerHTML = document.documentElement.outerHTML
+    return $done({ body: outerHTML })
 }
 
 
 (async () => {
     main().then(_ => {
-
     }).catch(error => {
         console.log(`[Error]: ${error?.message || error}`)
         $done({})
