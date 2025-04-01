d26f1be53aa72005e5864d09009448b30f1ba0eb
qsoyq <github@19940731.xyz>
2025-04-01 16:44:55 +0800

    update

--- config/stash/script/knit.bid/new.js
+++ config/stash/script/knit.bid/new.js
@@ -426,8 +426,8 @@ function parseArticleMaxPage(document) {
 
 function encodeKeyname(keyname) {
     // keyname = `xx.knit.bid-${article.category}-${article.href}-${article.time}`.replace('/', '-')
-    keyname = keyname.replace(/\[(.*)\]（.*）/, "$1")
-    keyname = keyname.replace(/(.*)\]（.*）/, "$1")
+    keyname = keyname.replace(/\[(.*)\]（.*?）(.*)/, "$1$2")
+    keyname = keyname.replace(/(.*)\]\(.*?\)(.*?)\(.*\)(.*?)/, "$1$2$3")
     keyname = keyname.replace('/', '-')
     // keyname = encodeURI(keyname)
     return keyname
