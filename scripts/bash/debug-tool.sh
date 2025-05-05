353107e287519676476070c2b83b4d3fb99b795a
qsoyq <github@19940731.xyz>
2025-05-05 21:21:29 +0800

    add stash/guardian.py

--- scripts/bash/debug-tool.sh
+++ scripts/bash/debug-tool.sh
@@ -19,5 +19,6 @@ curl -sL -o /usr/local/bin/pudp "https://raw.githubusercontent.com/qsoyq/shell/m
 curl -sL -o /usr/local/bin/encoder "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/tool/encoder.py?v=$(date +%s)"
 curl -sL -o /usr/local/bin/shcp "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/stash/dump-http-capture.py?v=$(date +%s)"
 curl -sL -o /usr/local/bin/keep-last-log "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/stash/keep_last_log.py?v=$(date +%s)"
+curl -sL -o /usr/local/bin/stash-guardian "https://raw.githubusercontent.com/qsoyq/shell/main/scripts/python/stash/guardian.py?v=$(date +%s)"
 
 chmod +x /usr/local/bin/*
---
+++ scripts/python/stash/guardian.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import os
+from datetime import datetime
+import typer
+import rich
+import rich.errors
+import httpx
+from pathlib import Path
+
+app = typer.Typer()
+
+
+def echo(*args, **kwargs):
+    if args:
+        arg = f"[{get_current_datetime_str()}] {args[0]}"
+        args = [arg, *args[1:]]
+    try:
+        rich.print(*args, **kwargs)
+    except rich.errors.MarkupError:
+        print(*args, **kwargs)
+
+
+def get_current_datetime_str() -> str:
+    return datetime.now().strftime(r"%Y-%m-%d %H:%M:%S")
+
+
+@app.command()
+def main(endpoint: str = typer.Option("http://127.0.0.1:9090/", "--endpoint"), cmd: Path = typer.Option("open /Applications/Stash.app/", "--cmd", help="启动命令")):
+    try:
+        resp = httpx.get(endpoint)
+        echo(resp.text)
+    except httpx.ConnectError:
+        exitcode = os.system(cmd)
+        if exitcode:
+            echo(f"execute {cmd} failed, exit: {exitcode}")
+
+
+if __name__ == "__main__":
+    app()
