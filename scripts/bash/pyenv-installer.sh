574c7bc5881f3c35e8f7f757b187da87a15c473b
qsoyq <github@19940731.xyz>
2025-05-21 11:16:04 +0800

    add pyenv-installer.sh

---
+++ scripts/bash/pyenv-installer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+curl https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
+
+touch ~/.bash_profile
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init - bash)"' >> ~/.bash_profile
+source ~/.bash_profile
