11b7df863ee184ddd03f317ff061174f419092ca
qsoyq <github@19940731.xyz>
2025-05-21 11:37:28 +0800

    update

--- scripts/bash/pyenv-installer.sh
+++ scripts/bash/pyenv-installer.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+if [ -e "/usr/bin/apt" ]; then
+    apt update && apt install build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev curl git \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+else
+    echo "未匹配的发行版"
+    exit 1
+fi
+
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
