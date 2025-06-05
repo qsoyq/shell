76e75ca90efec8eeb2cd4babf4ed9790ae95986b
qsoyq <github@19940731.xyz>
2025-06-05 13:38:53 +0800

    feat: add ping_statistics.py

---
+++ scripts/python/tool/ping_statistics.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from datetime import datetime
+import typer
+import rich
+import rich.errors
+from pythonping import ping, executor
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+from tabulate import tabulate
+
+
+app = typer.Typer()
+
+
+def echo(*args, **kwargs):
+    if args:
+        arg = f"[{get_current_datetime_str()}] {args[0]}"
+        args = [arg, *args[1:]]
+    try:
+        rich.print(*args, **kwargs)
+    except rich.errors.MarkupError:
+        print(*args, **kwargs)
+
+
+def get_current_datetime_str() -> str:
+    return datetime.now().strftime(r"%Y-%m-%d %H:%M:%S")
+
+
+@app.command()
+def main(ip: list[str] = typer.Argument(..., help="待测试 ip 列表"), count: int = typer.Option(4, "-c", "--count", help="ping次数")):
+    headers = ["ip", "min", "avg", "max", "loss"]
+    rows = []
+    with ThreadPoolExecutor() as pool:
+        global ping
+        ping = partial(ping, count=count)
+        res_li = pool.map(ping, ip)
+        for index, item in enumerate(res_li):
+            _ip = ip[index]
+            item: executor.ResponseList
+            rows.append((_ip, int(item.rtt_min * 1000), int(item.rtt_avg * 1000), int(item.rtt_max * 1000), item.packet_loss))
+    rows = sorted(rows, key=lambda x: (x[4], x[2]))
+    output = tabulate(rows, headers=headers, tablefmt="pipe")
+
+    print(output)
+
+
+if __name__ == "__main__":
+    app()
